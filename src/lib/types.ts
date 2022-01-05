08838c46f4189ccb533bdb965bb7b2bcd195bff3
Dean Lozo <dean.lozo+github@gmail.com>
2022-01-05 21:15:23 +0100

    Added automatic check if day is a public or bank holiday. (#12)

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,13 @@
 export type DiffRequest = {
-    startDate: string;
-    endDate: string;
-    workdayLength: number;
-    workspaceId: string;
-    apiKey: string;
-}
+	startDate: string;
+	endDate: string;
+	workdayLength: number;
+	workspaceId: string;
+	apiKey: string;
+};
 
 export type DiffResponse = {
-    workedHours: number
-    expectedHours: number
-    diffHours: number
-}
+	workedHours: number;
+	expectedHours: number;
+	diffHours: number;
+};
