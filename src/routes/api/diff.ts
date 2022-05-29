be7951f42d20b719730d93eacf805f28218ed6dd
Dean Lozo <dean.lozo+github@gmail.com>
2022-05-29 20:34:47 +0000

    Remove clockify-ts dependency

--- src/routes/api/diff.ts
+++ src/routes/api/diff.ts
@@ -1,26 +1,25 @@
-import Clockify, { RequestSummaryReportGroupsEnum } from 'clockify-ts';
-import type { RequestSummaryReportType } from 'clockify-ts';
 import type { DiffRequest, DiffResponse } from '$lib/types';
 import { workDays } from '$lib/timeUtils';
 
 export async function post({ request }) {
+	const baseUrl = "https://reports.api.clockify.me/v1"
 	const clockifyRequest = await request.json() as DiffRequest;
 	const startDate = new Date(clockifyRequest.startDate);
 	const endDate = new Date(clockifyRequest.endDate);
 	endDate.setHours(23, 59, 59);
 
-	const clockify = new Clockify(clockifyRequest.apiKey);
-	const summaryQuery: RequestSummaryReportType = {
-		dateRangeStart: new Date(clockifyRequest.startDate),
-		dateRangeEnd: new Date(endDate),
-		summaryFilter: {
-			groups: [RequestSummaryReportGroupsEnum.user]
-		}
-	};
-	const report = await clockify.workspaces
-		.withId(clockifyRequest.workspaceId)
-		.reports.summary.post(summaryQuery);
+	const postBody = `{"dateRangeStart": "${startDate.toISOString()}","dateRangeEnd": "${endDate.toISOString()}","summaryFilter": {"groups": ["USER"]},"exportType": "JSON"}`
 
+	const summaryResponse = await fetch(`${baseUrl}/workspaces/${clockifyRequest.workspaceId}/reports/summary`, {
+		method: 'POST',
+		body: postBody,
+		headers: {
+			'X-Api-Key': clockifyRequest.apiKey,
+			'content-type': 'application/json'
+		}
+	});
+
+	const report = await summaryResponse.json()
 	const workedHours = report.totals[0].totalTime / 60 / 60;
 	const expectedHours = workDays(startDate, endDate) * clockifyRequest.workdayLength;
 	const diffHours = workedHours - expectedHours;
