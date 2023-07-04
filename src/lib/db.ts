d97bdacab6f063141fe98023bf0990eec28a9fbd
Dean Lozo <dean.lozo@technipfmc.com>
2023-07-04 13:40:16 +0200

    Remvoed old file

--- src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/db.ts
-import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-
-export const supabaseClient = createClient(
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_ANON_KEY
-);
