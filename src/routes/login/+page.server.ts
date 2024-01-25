c8a011200bbbd07fdb5e58780d03e7884f3d92a3
Dean Lozo <dean.lozo@technipfmc.com>
2024-01-25 16:11:34 +0100

    Implemented working magic link

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,29 @@
+import { fail } from '@sveltejs/kit';
+
+export const actions  = {
+	default: async ({ url, request,locals: { supabase } }) => {
+
+		const formData = await request.formData();
+		const email = formData.get('email') as string;
+
+		const { error } = await supabase.auth.signInWithOtp({
+			email,
+			options: {
+				shouldCreateUser: false,
+				emailRedirectTo: `${url.origin}/`
+			}
+		});
+
+		if (error) {
+			return fail(400, {
+				error: error.message,
+				values: { email }
+			});
+		}
+
+		return {
+			success: true,
+			message: 'Success! Magic link sent, check your email.'
+		};
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
 	import { IconMail } from '@tabler/icons-svelte';
+	import { applyAction, enhance, type SubmitFunction } from '$app/forms';
+
+	export let form;
+
+	const handleSubmit: SubmitFunction = () => {
+		return async ({ result }) => {
+			await applyAction(result);
+		};
+	};
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center max-w-[50%] gap-x-3">
-	<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-		<div class="input-group-shim"><IconMail /></div>
-		<input type="email" placeholder="name@email.com" />
-		<button class="variant-filled-secondary">Send Magic Link</button>
-	</div>
+	<form method="POST" use:enhance={handleSubmit}>
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">
+				<IconMail />
+			</div>
+			<input name="email" type="email" placeholder="name@email.com" />
+			<button class="variant-filled-secondary" type="submit">Send Magic Link</button>
+		</div>
+	</form>
+
+	{#if form?.error}
+		<div class="alert-message">
+			<p>{form.error}</p>
+		</div>
+	{/if}
+	{#if form?.message}
+		<div class="alert-message">
+			<p>{form.message}</p>
+		</div>
+	{/if}
 </div>
